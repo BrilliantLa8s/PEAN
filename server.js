593938067d378aefa44fc7c8ca3dd152aa8703cf
Ricky Brown <ricky@brilliantdeveloping.com>
2016-06-12 15:39:33 -0700

    sequelize init, use postgresql db

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var models = require('./api/models');
 var app = express();
 
 app.use(express.static('client'));
 
 app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
+models.sequelize.sync().then(function () {
+  var server = app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + server.address().port);
+  });
 });
