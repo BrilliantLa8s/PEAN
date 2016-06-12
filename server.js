9c9bdc1d316a54849ad8160f52fc7aebde06eb35
Ricky Brown <ricky@brilliantdeveloping.com>
2016-06-12 15:27:13 -0700

    use client static directory

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
+app.use(express.static('client'));
 
 app.set('port', process.env.PORT || 3000);
 
