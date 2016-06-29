b1380932de1b864bbb6e535c2aafff5aeed0e95b
Ricky Brown <ricky@brilliantdeveloping.com>
2016-06-29 00:16:06 -0700

    create db:reset & db:seed tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var shell = require('gulp-shell');
 var cleanCSS = require('gulp-clean-css');
 var concat = require('gulp-concat');
 var nodemon = require('gulp-nodemon');
@@ -8,6 +9,7 @@ var proxy = require('http-proxy-middleware');
 var run = require('run-sequence');
 var del = require('del');
 
+var config = require('./config/config.json');
 var dir = './client/'
 
 var paths = {
@@ -68,6 +70,13 @@ gulp.task('serve', function () {
     }
   });
 });
+
+gulp.task('db:reset', shell.task([
+  'psql -U postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = \'cxp_development\' AND pid <> pg_backend_pid()"',
+  'psql -U postgres -c "DROP DATABASE IF EXISTS '+config.development.database+'"',
+  'psql -U postgres -c "CREATE DATABASE '+config.development.database+' WITH OWNER = postgres"',
+  'sequelize db:migrate'
+]));
 
 gulp.task('build', ['clean', 'build:js', 'build:css']);
 gulp.task('default', ['build']);
