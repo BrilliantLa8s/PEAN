9070379190fdb3ec744375492d04b0078118c9ae
Ricky Brown <ricky.brown@indigoslate.com>
2016-08-06 23:14:50 -0700

    use body parser, add catchall route

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-var express = require('express');
+'use strict';
+
+var bodyParser = require('body-parser');
 var models = require('./api/models');
+var express = require('express');
 var app = express();
 
-app.use(express.static('client'));
-app.use('/api/vendor', express.static('bower_components'));
+// Parse body as JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
+// Client App Directory
+app.use(express.static('client'));
 
 // API Controllers
 app.use('/api/posts', require('./api/controllers/posts'));
 
-// Serve app
+// Catchall Route
+app.use(function(req, res){
+  res.sendFile(__dirname + '/client/index.html')
+});
+
+// Serve Application
 app.set('port', process.env.PORT || 3000);
 
 models.sequelize.sync().then(function () {
