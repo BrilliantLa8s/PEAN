6d40031bef9ab484a99ebffd6e099eb91d232131
Ricky Brown <ricky.brown@indigoslate.com>
2016-08-19 15:55:06 -0700

    create global resource factory

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -4,12 +4,14 @@ var model = require('../models/index');
 var express = require('express');
 var router = express.Router();
 
+// get all posts
 router.get('/', function(req, res) {
   model.Post.all().then(function(posts) {
     res.status(200).send(posts);
   });
 });
 
+// get one post
 router.get('/:id', function(req, res) {
   model.Post.find({
     where: { id: req.params.id }
