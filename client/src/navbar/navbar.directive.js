cd8679fbc41609ccd2cd51507ed1f825a50f62b5
Ricky Brown <ricky.brown@indigoslate.com>
2016-08-20 11:48:18 -0700

    create sidemenus for mobile & small screens

--- client/src/navbar/navbar.directive.js
+++ client/src/navbar/navbar.directive.js
@@ -1,14 +1,59 @@
-app.controller('NavbarCtrl', function($scope) {
+app.controller('NavbarCtrl', function($scope, $timeout, $mdSidenav, $log) {
+  var buildDelayedToggler, buildToggler, debounce
+  debounce = function(func, wait, context) {
+    var timer;
+    timer = void 0;
+    return function() {
+      var context;
+      var args;
+      context = $scope;
+      args = Array.prototype.slice.call(arguments);
+      $timeout.cancel(timer);
+      timer = $timeout((function() {
+        timer = void 0;
+        func.apply(context, args);
+      }), wait || 10);
+    };
+  };
+  buildDelayedToggler = function(navID) {
+    return debounce((function() {
+      $mdSidenav(navID).toggle().then(function() {
+        $log.debug('toggle ' + navID + ' is done');
+      });
+    }), 200);
+  };
+  buildToggler = function(navID) {
+    return function() {
+      $mdSidenav(navID).toggle().then(function() {
+        $log.debug('toggle ' + navID + ' is done');
+      });
+    };
+  };
+  $scope.toggleLeft = buildDelayedToggler('left');
+  $scope.toggleRight = buildToggler('right');
+  $scope.isOpenRight = function() {
+    return $mdSidenav('right').isOpen();
+  };
+  $scope.closeLeft = function() {
+    $mdSidenav('left').close().then(function() {
+      $log.debug('close LEFT is done');
+    });
+  };
+  $scope.closeRight = function() {
+    $mdSidenav('right').close().then(function() {
+      $log.debug('close RIGHT is done');
+    });
+  };
   $scope.mainmenu = [
     {
       link: 'Home',
       state: 'main',
-      icon: ''
+      icon: 'home'
     },
     {
       link: 'Posts',
       state: 'posts',
-      icon: ''
+      icon: 'description'
     }
   ];
   // Authmenu
