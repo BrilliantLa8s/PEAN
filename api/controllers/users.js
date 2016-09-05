30bdbd6f472d7f1c1430e4af485f71a2784a09e3
Ricky Brown <ricky.brown@indigoslate.com>
2016-09-04 19:57:02 -0700

    create user update api route

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var model = require('../models/index');
+var auth = require('../helpers/auth');
 var express = require('express');
 var router = express.Router();
 
@@ -22,6 +23,26 @@ router.get('/profile', function(req, res){
   }).then(function(user) {
     res.status(200).send(user);
   });
+});
+
+router.put('/update', function(req, res){
+  if(req.body.type){
+    model.User.findOne({
+      where:{id:req.user.id}
+    }).then(function(user) {
+      auth.update(req.user, user.password, req.body)
+      .then(function(changes){
+        user.updateAttributes(changes)
+        .then(function(user){
+          res.status(200).send(req.user)
+        }).catch(function(err){
+          res.status(404).send(err.errors[0].message)
+        })
+      }).catch(function(err){
+        res.status(404).send(err)
+      })
+    });
+  } else {res.status(400).send('missing type')}
 });
 
 module.exports = router;
