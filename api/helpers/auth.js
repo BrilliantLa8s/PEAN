622d66b0cf4e84eeea2bb5b34845826aa62b511e
Ricky Brown <ricky.brown@indigoslate.com>
2016-09-04 19:52:56 -0700

    create update and validate auth methods

--- api/helpers/auth.js
+++ api/helpers/auth.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var config = require('../../config/app');
+var vars = require('../../config/vars');
 var error = require('../../config/errors');
 
 var jwt = require('jsonwebtoken');
@@ -15,7 +16,7 @@ auth.login = function(user, formPassword) {
   var defer = require('q').defer();
   if(bcrypt.compareSync(formPassword, user.password)) {
     delete user.password
-    var token = jwt.sign(user, config.secret, {
+    var token = jwt.sign({info:user}, config.secret, {
       expiresIn: 1440 // expires in 24 hours
     });
     defer.resolve(token);
@@ -25,14 +26,40 @@ auth.login = function(user, formPassword) {
   return defer.promise;
 }
 
-// Authenticate Requests
+auth.update = function(user, password, body) {
+  var defer = require('q').defer();
+  if(body.type === 'attributes') {defer.resolve(body);}
+  else if(bcrypt.compareSync(body.currentPassword, password)) {
+    if(body.type === 'email'){
+      user.email = body.email;
+      user.token = jwt.sign({info:user}, config.secret, {
+        expiresIn: 1440 // expires in 24 hours
+      });
+      defer.resolve(body);
+    } else if(body.type === 'password'){
+      if(auth.validate('password', body.newPassword)){
+        body.password = bcrypt.hashSync(body.newPassword, vars.bcrypt.salt);
+        defer.resolve(body)
+      } else {
+        defer.reject(error.auth.invalid.password);
+      }
+    } else {
+      defer.reject(error.auth.unable);
+    }
+  } else {
+    defer.reject(error.auth.change);
+  }
+  return defer.promise;
+}
+
+// Authenticate Requestspppppppp[]
 auth.authenticateRequests = function(req, res, next){
   // Check for auth token on other api requests
   var token = req.headers.token
   if (token) {
     jwt.verify(token, config.secret, function(err, obj) {
       if (err) {res.status(401).send(err);}
-      else {req.user = obj; next();};
+      else {req.user = obj.info; next();};
     });
   } else {
     if (process.env.DEV_USER) {
@@ -56,5 +83,10 @@ auth.authenticateRequests = function(req, res, next){
     }
   }
 };
+
+auth.validate = function(type, string){
+  if(type==='password')
+  return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,15}$/.test(string);
+}
 
 module.exports = auth;
